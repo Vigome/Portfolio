0edf8492d1e95cdf64c8be7df1444da9e785586d
Michal <michal.pyszynski93@gmail.com>
2019-06-28 12:51:56 +0200

    Updates

---
+++ precache-manifest.102a7de580116968bbd6d42c4fc17325.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651b0140208ff34bc9e49f38a31d69f7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "060d2dca1de2bd182ea0",
+    "url": "/Portfolio/static/css/main.d5fa9caf.chunk.css"
+  },
+  {
+    "revision": "e5a04d12960cc09e49a5",
+    "url": "/Portfolio/static/js/2.aa2deb85.chunk.js"
+  },
+  {
+    "revision": "060d2dca1de2bd182ea0",
+    "url": "/Portfolio/static/js/main.dd66b8cf.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "f9417942c22b97270f2e8e51dd9de711",
+    "url": "/Portfolio/static/media/56325290_321212165100586_5703099949223247872_n.f9417942.png"
+  },
+  {
+    "revision": "cbc79f6e30966969ebbbc1c1a8d4831a",
+    "url": "/Portfolio/static/media/benjaminrobyn-jespersen-700466-unsplash.cbc79f6e.jpg"
+  },
+  {
+    "revision": "7805af5bb4e771a7b4ae4a6418f88d35",
+    "url": "/Portfolio/static/media/calculator.7805af5b.png"
+  },
+  {
+    "revision": "232861b903a83b4d66051f7d7963251c",
+    "url": "/Portfolio/static/media/e-commerce.232861b9.png"
+  },
+  {
+    "revision": "984be8b9c9ca581010a2cc210eeb592e",
+    "url": "/Portfolio/static/media/jquery.984be8b9.png"
+  },
+  {
+    "revision": "a60a5ef977825ff141534803c560a546",
+    "url": "/Portfolio/static/media/portfolio.a60a5ef9.jpg"
+  },
+  {
+    "revision": "b51d1861e7fe5782d20d5455c7c9f1f4",
+    "url": "/Portfolio/static/media/redux.b51d1861.png"
+  },
+  {
+    "revision": "cfe84e1a4d032570d36864781d423589",
+    "url": "/Portfolio/static/media/travel-agency.cfe84e1a.jpg"
+  }
+]);
